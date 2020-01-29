563eb4d89513e13bb71fe06489de8fe77949ac0f
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-01-28 21:20:04 -0300

    feat: Persistindo dados da autenticação com redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persisReducers from './persisReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -12,8 +15,9 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persisReducers(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
